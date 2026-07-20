9e868d34d957b3e08d290617969d36dffc4e9c84
50ans <vctr@ik.me>
2026-07-20 15:27:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://gsnrrrupbjfgdlbghyuy.supabase.co";
 
-const SUPABASE_KEY = "Tsb_publishable_NbYNMTN7qdT7vQWGc2oUAA_CpF1Hl-0";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzbnJycnVwYmpmZ2RsYmdoeXV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NDYzODksImV4cCI6MjEwMDEyMjM4OX0.fN6Ue2NP0fXP53pUCBGTEjhFHqacEJGGwD1EmvDHA24";
 
 
 window.supabaseClient = supabase.createClient(
