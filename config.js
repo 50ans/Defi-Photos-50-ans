c3e543e17039d58cbc6c5ef939f29954f9dfe250
50ans <vctr@ik.me>
2026-07-20 15:23:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const SUPABASE_URL = "https://gsnrrrupbjfgdlbghyuy.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_NbYNMTN7qdT7vQWGc2oUAA_CpF1Hl-0";
+const SUPABASE_KEY = "Tsb_publishable_NbYNMTN7qdT7vQWGc2oUAA_CpF1Hl-0";
 
-const supabaseClient = supabase.createClient(
+window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
