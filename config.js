194c591613334980b34b62231d0e65c678d0e8d9
50ans <vctr@ik.me>
2026-07-20 15:34:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,23 @@
+console.log("🔵 Chargement config.js");
+
+
 const SUPABASE_URL = "https://gsnrrrupbjfgdlbghyuy.supabase.co";
+
 
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzbnJycnVwYmpmZ2RsYmdoeXV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NDYzODksImV4cCI6MjEwMDEyMjM4OX0.fN6Ue2NP0fXP53pUCBGTEjhFHqacEJGGwD1EmvDHA24";
 
+
 window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_KEY
+    SUPABASE_KEY,
+    {
+        auth: {
+            persistSession: false,
+            autoRefreshToken: false
+        }
+    }
 );
 
+
 console.log("✅ Supabase connecté");
+console.log("Client Supabase :", window.supabaseClient);
