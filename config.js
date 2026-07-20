d879d1b03ec4acfe9de3b721799b821bea5600c7
50ans <vctr@ik.me>
2026-07-20 15:25:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,12 @@ const SUPABASE_URL = "https://gsnrrrupbjfgdlbghyuy.supabase.co";
 
 const SUPABASE_KEY = "Tsb_publishable_NbYNMTN7qdT7vQWGc2oUAA_CpF1Hl-0";
 
+
 window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
 
+
 console.log("✅ Supabase connecté");
+console.log(window.supabaseClient);
